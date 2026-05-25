3ed4589ffd989fa39238736776e37df43bbf025d
Til Freitag <104679877+Til2ktyp@users.noreply.github.com>
2026-05-25 20:17:02 +0200

    update tester.py

---
+++ updater.py
@@ -0,0 +1,20 @@
+import time
+import secrets
+from time import sleep
+import os
+
+print("BlopperBold Pulse Updater (C)")
+
+sleep(1)
+
+os.system("taskkill /im node.exe /f")
+
+sleep(1)
+
+os.system("cd Blopperbold-Nexus")
+
+os.system("git pull")
+
+os.system("C:\Users\Neu\Desktop\startserver.bat")
+
+print("Update Abgeschlossen")
